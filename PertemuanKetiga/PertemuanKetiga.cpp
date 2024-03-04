e873a1212a91c4bd7b4bf7997825f6e27c40649a
Ridhorzqullah <ridhorizqullah3@gmail.com>
2024-03-04 12:04:11 +0700

    Add project files.

---
+++ PertemuanKetiga/PertemuanKetiga.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	srand(time(0));
+
+
+	int nBilA, nBilB;
+	string status;
+	
+	nBilA = rand() % 11;
+	nBilB = rand() % 11;
+	
+	if (nBilB == nBilA) {
+		status = "bilangan sama";
+	}
+	else  if (nBilA > nBilB) {
+		status = " Bilangan A lebih kecil dari bilangan B";
+	}
+	else
+	{
+		status = "Bilangan A lebih kecil dari Bilangan B";
+	}
+		
+	cout << "Bilangan A = " << nBilA << endl;
+	cout << "Bilangan A = " << nBilA << endl;
+	cout << "Status Bilangan = " << status << endl;
+
+	system("pause");
+}
